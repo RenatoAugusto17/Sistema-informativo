771c31e81795f9161f43e962be136f5874a2971f
RenatoAugusto17 <169068426+RenatoAugusto17@users.noreply.github.com>
2024-06-13 03:10:47 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,49 @@
+ import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner SC = new
+      Scanner(System.in);
+    System.out.println("A seguir o sistema vai coletar informações básicas sobre um aluno do 1B e falar todos:");
+    System.out.println("");
+    System.out.println("Nome:");
+    String nome = SC.next();
+    System.out.println("");
+System.out.println("Sobrenome:");
+    String sobrenome = SC.next();
+    System.out.println("");
+    System.out.println("Idade:");
+    String idade = SC.next();
+    System.out.println("");
+    System.out.println("Altura:");
+    String altura = SC.next();
+    System.out.println("");
+    System.out.println("Massa corporal:");
+    String massa = SC.next();
+    System.out.println("");
+    System.out.println("Cálculo do IMC(Índice de Massa Corporal):");
+    double Massa = 55;
+      double Altura = 1.64;
+      
+      System.out.println("55 kg ÷ 1,64 de altura^(2)");
+    System.out.println("");
+    System.out.println("1,64 × 1,64 = 2,6...");
+    System.out.println("");
+    System.out.println("55 ÷ 2,6... = 20,4");
+    System.out.println("");
+    System.out.println("Nome:");
+    System.out.println("Renato Augusto");
+    System.out.println("");
+    System.out.println("Idade:");
+    System.out.println("15 anos");
+    System.out.println("");
+    System.out.println("Altura:");
+    System.out.println("1,64m");
+    System.out.println("");
+    System.out.println("Massa:");
+      System.out.println("55kg");
+    System.out.println("");
+    System.out.println("IMC:");
+    System.out.println("20,4");
+  }
+}
